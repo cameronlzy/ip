6cd8b5b31fce9bace1f4ef85254e2c86ffe643ea
Cameron <cameronlzy@gmail.com>
2025-08-18 15:08:17 +0800

    Level 3. Mark as Done

--- src/main/java/Rex.java
+++ src/main/java/Rex.java
@@ -6,7 +6,8 @@ public class Rex {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String chatbotName = "Rex";
-        List<String> list = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm " + chatbotName);
         System.out.println("What can I do for you? \n");
@@ -22,12 +23,37 @@ public class Rex {
                 break;
             } else if(input.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
-                for(int i = 0; i < list.size(); i++) {
-                    System.out.println(i + 1 + ". " + list.get(i));
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
                 }
                 System.out.println("____________________________________________________________");
+            } else if (input.startsWith("mark ")) {
+                try {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    Task t = tasks.get(index);
+                    t.markDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + t);
+                    System.out.println("____________________________________________________________");
+                } catch (Exception e) {
+                    System.out.println("Invalid task number!");
+                }
+            } else if (input.startsWith("unmark ")) {
+                try {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    Task t = tasks.get(index);
+                    t.markUndone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + t);
+                    System.out.println("____________________________________________________________");
+                } catch (Exception e) {
+                    System.out.println("Invalid task number!");
+                }
             } else {
-                list.add(input);
+                tasks.add(new Task(input));
                 System.out.println("____________________________________________________________");
                 System.out.println("added: " + input);
                 System.out.println("____________________________________________________________");
