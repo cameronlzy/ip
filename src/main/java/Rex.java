2b681eb9c7ae8603d9e54755a2ada20bdef1248d
Cameron <cameronlzy@gmail.com>
2025-08-18 14:58:41 +0800

    Level 1. Echo

--- src/main/java/Rex.java
+++ src/main/java/Rex.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Rex {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String chatbotName = "Rex";
+        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm " + chatbotName);
         System.out.println("What can I do for you? \n");
+        System.out.println("____________________________________________________________");
 
-        System.out.println("Bye. Hope to see you again soon!");
+        while(true) {
+            String input = sc.nextLine();
+
+            if(input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+        sc.close();
     }
 }
