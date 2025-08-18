0f505a9d4b25787c4101b87eb174ca8113cd1566
Cameron <cameronlzy@gmail.com>
2025-08-18 14:53:35 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Rex.java
@@ -0,0 +1,9 @@
+public class Rex {
+    public static void main(String[] args) {
+        String chatbotName = "Rex";
+        System.out.println("Hello! I'm " + chatbotName);
+        System.out.println("What can I do for you? \n");
+
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
