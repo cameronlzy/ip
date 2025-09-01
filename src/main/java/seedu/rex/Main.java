b259721306753ac89568d2d6d813136d7458cd8c
Cameron <cameronlzy@gmail.com>
2025-09-01 19:37:17 +0800

    Add images to GUI

--- src/main/java/seedu/rex/Main.java
+++ src/main/java/seedu/rex/Main.java
@@ -6,9 +6,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Region;
 
+import java.util.Objects;
 
 public class Main extends Application {
 
@@ -17,17 +21,29 @@ public class Main extends Application {
     private TextField userInput;
     private Button sendButton;
     private Scene scene;
+    private Image userImage;
+    private Image rexImage;
+
+
+
+    AnchorPane mainLayout = new AnchorPane();
+//    private Image rexImage = new Image(this.getClass().getResourceAsStream("/main/resources/images/rexImage.jpg"));
 
     @Override
     public void start(Stage stage) {
         //Setting up required components
-
+        var userUrl = Objects.requireNonNull(getClass().getResource("/images/userImage.jpg"), "Missing /images/userImage.jpg");
+        var rexUrl = Objects.requireNonNull(getClass().getResource("/images/rexImage.jpg"), "Missing /images/rexImage.jpg");
+        Image userImage = new Image(userUrl.toExternalForm());
         scrollPane = new ScrollPane();
         dialogContainer = new VBox();
         scrollPane.setContent(dialogContainer);
 
         userInput = new TextField();
         sendButton = new Button("Send");
+
+        DialogBox dialogBox = new DialogBox("Hello!", userImage);
+        dialogContainer.getChildren().addAll(dialogBox);
 
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
@@ -36,6 +52,34 @@ public class Main extends Application {
 
         stage.setScene(scene);
         stage.show();
+
+        stage.setTitle("Rex");
+        stage.setResizable(false);
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
+
+        mainLayout.setPrefSize(400.0, 600.0);
+
+        scrollPane.setPrefSize(385, 535);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        scrollPane.setVvalue(1.0);
+        scrollPane.setFitToWidth(true);
+
+        dialogContainer.setPrefHeight(Region.USE_COMPUTED_SIZE);
+
+        userInput.setPrefWidth(325.0);
+
+        sendButton.setPrefWidth(55.0);
+
+        AnchorPane.setTopAnchor(scrollPane, 1.0);
+
+        AnchorPane.setBottomAnchor(sendButton, 1.0);
+        AnchorPane.setRightAnchor(sendButton, 1.0);
+
+        AnchorPane.setLeftAnchor(userInput, 1.0);
+        AnchorPane.setBottomAnchor(userInput, 1.0);
 
         //More code to be added here later
     }
